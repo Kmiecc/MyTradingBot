b868e48eff31aa3bd2960bbd1fe132ef2d02878f
mkmieck1@stu.vistula.edu.pl <mkmieck1@stu.vistula.edu.pl>
2024-03-03 13:52:35 +0100

    .

--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/TraDINGbOT.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/TraDINGbOT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TradingBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+858d561669841f68d487bf065bc5d7459b0483d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64fc87bbd4662b41011072be367cc4af0458676d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TradingBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TradingBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
